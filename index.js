6ac486e9bdf9dcee64f070c844288201e8a01b07
Benjamin Kerns <benkerns09@Benjamins-MBP.attlocal.net>
2018-07-01 19:33:59 -0400

    GolfPlayed1

---
+++ index.js
@@ -0,0 +1,12 @@
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+
+    var firebaseRef = firebase.database().ref();
+
+    var messageText = mainText.value;
+
+    firebaseRef.push("Text").set(messageText);
+
+}
